97725bb18c08b373bf143fecfeaf5e3156819cff
Guilherme Bezerra <gbsantos.it@gmail.com>
2022-10-14 00:03:54 -0300

    🏁 finish ignite lab

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,4 +1,5 @@
 import { FormEvent, useState } from 'react';
+import axios from 'axios';
 
 import { Button } from '../components/Button';
 import { Checkbox } from '../components/Checkbox';
@@ -14,8 +15,13 @@ import '../styles/global.css';
 export function SignIn() {
   const [isUserSignedIn, setIsUserSignedIn] = useState(false);
 
-  function handleSignIn(event: FormEvent) {
+  async function handleSignIn(event: FormEvent) {
     event.preventDefault();
+
+    await axios.post('/sessions', {
+      email: 'gbsantos.it@gmail.com',
+      password: '123456789'
+    });
 
     setIsUserSignedIn(true);
   }
